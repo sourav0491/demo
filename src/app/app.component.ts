31742458541379eeb8bba2f2b9d55c71b3791d40
Sourav Roy <sourav.roy@rsagroup.ca>
2019-04-01 18:08:34 +0530

    final demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild} from '@angular/core';
+import {ChildComponent} from './child/child.component';
 
-@Component({
+@Component({ 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Component communication in angular';
-  parent_param="this parameter is from parent";
-  childMessage:string="no message from child yet";
+export class AppComponent  {
+  title = 'Parent Component';
+  parent_param="";
+  childMessage:string="";
+  @ViewChild(ChildComponent) childComponenet: ChildComponent;
 
   updateChildMsg(msg){
     this.childMessage=msg;
+  }
+
+  communicate(msg){
+    //this.parent_param=msg.value;
+    this.childComponenet.parent_param=msg.value;
   }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -4,22 +4,23 @@ import {DataCommunicationService} from '../data-communication.service';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'] 
 })
 export class ChildComponent implements OnInit {
   @Input() parent_param:string;
   @Output() childEvent = new EventEmitter<String>();
-  childMsg:string="Message from child";
-  msgFromUnrelated:String="No message yet from unrelated component";
+  msg:string;
+  msgFromUnrelated:String;
 
   constructor(private _dataCommunicationService:DataCommunicationService) {
     this._dataCommunicationService.getMessage().subscribe(msg => this.msgFromUnrelated=msg);
    }
 
   ngOnInit() {
+    this.parent_param="";
   }
 
   communicate(){
-    this.childEvent.emit(this.childMsg);
+    this.childEvent.emit(this.msg);
   }
 }
